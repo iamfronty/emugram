3cca0e0eb0f45c5fe191835cde6ce85b5b8f9295
Vlad B <me@fronty.ws>
2024-12-20 15:05:28 +0300

    fix home img

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,16 +7,9 @@ import { engines } from "@/store";
   <main class="home">
     <h1 class="text-3xl font-500 w-full text-center pt-20 pb-8">Retrogram</h1>
     <div class="flex flex-col gap-2">
-      <router-link
-        v-for="item in engines"
-        :key="item.id"
-        :to="'/engine/' + item.id"
-        class="flex flex-row items-center gap-4 p-2 bg-neutral-800 rounded-md"
-      >
-        <img
-          :src="'/src/assets/img/' + item.id + '.png'"
-          class="h-20 w-20 object-fill"
-        />
+      <router-link v-for="item in engines" :key="item.id" :to="'/engine/' + item.id"
+        class="flex flex-row items-center gap-4 p-2 bg-neutral-800 rounded-md">
+        <img :src="`/img/${item.id}.png`" class="h-20 w-20 object-fill" />
         {{ item.name }}
       </router-link>
     </div>
